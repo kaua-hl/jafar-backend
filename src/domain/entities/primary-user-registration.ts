7183149014d69585ce39ee1a9cd4d4bb9f45f3c9
kauã <profissional.kaua@gmail.com>
2023-07-03 12:55:32 -0300

    feat: user model account

--- src/domain/entities/primary-user-registration.ts
+++ src/domain/entities/primary-user-registration.ts
@@ -1,7 +1,7 @@
-type PrimaryUserRegistration = {
-    name: string
-    lastname: string
-    cpf: string
-    email: string
-    password: string
-}
+export type PrimaryUserRegistration = {
+	name: string;
+	lastname: string;
+	cpf: string;
+	email: string;
+	password: string;
+};
