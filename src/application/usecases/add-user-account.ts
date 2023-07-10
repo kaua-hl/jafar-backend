6910d5a5253f71919ab1439f5e8c1d49cd94c4c9
kauã <profissional.kaua@gmail.com>
2023-07-10 11:04:59 -0300

    feat: add AddUserAccount with new features, validation and hash password

--- src/application/usecases/add-user-account.ts
+++ src/application/usecases/add-user-account.ts
@@ -1,26 +1,29 @@
-import { PrimaryUserRegistration } from "../../domain/entities/primary-user-registration";
 import { AddAccount } from "../../domain/usecases/add-account";
-import { AddUserAccountRepository } from "../services/add-user-account-repository";
-import { FindAccountByEmailRepository } from "../services/find-account-by-email-repository";
+import { AddAccountRepository } from "../services/add-user-account-repository";
+import { CheckAccountRepository } from "../services/check-user-exists-repository";
+import { Hasher } from "../services/hasher";
 
 export class AddUserAccount implements AddAccount {
-	private readonly findAccountByEmailRepository: FindAccountByEmailRepository;
-	private readonly addUserAccountRepository: AddUserAccountRepository;
+	private readonly checkAccountRepository: CheckAccountRepository;
+	private readonly hasher: Hasher;
+	private readonly addAccountRepository: AddAccountRepository;
 
 	constructor(
-		findAccountByEmailRepository: FindAccountByEmailRepository,
-		addUserAccountRepository: AddUserAccountRepository
+		checkAccountRepository: CheckAccountRepository,
+		addAccountRepository: AddAccountRepository,
+		hasher: Hasher
 	) {
-		this.findAccountByEmailRepository = findAccountByEmailRepository;
-		this.addUserAccountRepository = addUserAccountRepository;
+		this.checkAccountRepository = checkAccountRepository;
+		this.addAccountRepository = addAccountRepository;
+		this.hasher = hasher;
 	}
 
-	async add(data: PrimaryUserRegistration): Promise<any> {
-		const verifyUserExists = await this.findAccountByEmailRepository.findByEmail(data.email);
+	async add(accountData: AddAccount.Params): Promise<AddAccount.Result> {
+		const verifyUserExists = await this.checkAccountRepository.checkByEmail(accountData.email);
 		let isValid = false;
 		if (!verifyUserExists) {
-			const account = await this.addUserAccountRepository.add(data);
-			isValid = account;
+			const hashedPassword = await this.hasher.hash(accountData.password);
+			isValid = await this.addAccountRepository.add({ ...accountData, password: hashedPassword });
 		}
 		return isValid;
 	}
