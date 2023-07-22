ee43d3c18c4e2fc24daa931fb20a1547224a60e4
kauã <profissional.kaua@gmail.com>
2023-07-21 22:06:57 -0300

    feat: add UserRepository

--- src/infra/mongodb/repositories/mongodb-user-repository.ts
+++ src/infra/mongodb/repositories/mongodb-user-repository.ts
@@ -1,19 +1,40 @@
-import { AddUserAccountRepository } from "../../../application/services/add-user-account-repository";
-import { FindAccountByEmailRepository } from "../../../application/services/find-account-by-email-repository";
-import { PrimaryUserRegistration } from "../../../domain/entities/primary-user-registration";
+import { AddAccountRepository } from "../../../application/services/add-user-account-repository";
+import { CheckAccountRepository } from "../../../application/services/check-user-exists-repository";
+import { LoadAccountByEmailRepository } from "../../../application/services/load-account-by-email-repository";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class UserRepository implements AddUserAccountRepository, FindAccountByEmailRepository {
-	async add(data: PrimaryUserRegistration): Promise<any> {
+export class UserRepository implements AddAccountRepository, LoadAccountByEmailRepository, CheckAccountRepository {
+	async add(data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
 		const collection = await MongoHelper.getCollection("accounts");
 		const account = await collection.insertOne(data);
-		const result = await collection.findOne(account.insertedId);
-		return result;
+		return account.insertedId !== null;
 	}
 
-	async findByEmail(email: string): Promise<any> {
+	async checkByEmail(email: string): Promise<any> {
 		const collection = await MongoHelper.getCollection("accounts");
-		const user = await collection.findOne({ email });
-		return user;
+		const user = await collection.findOne(
+			{ email },
+			{
+				projection: {
+					_id: 1,
+				},
+			}
+		);
+		return user !== null;
+	}
+
+	async loadByEmail(email: string): Promise<LoadAccountByEmailRepository.Result> {
+		const collection = await MongoHelper.getCollection("accounts");
+		const account = await collection.findOne(
+			{ email },
+			{
+				projection: {
+					_id: 1,
+					password: 1,
+				},
+			}
+		);
+
+		return MongoHelper.formatCollection(account);
 	}
 }
