3246eb2c818c8c0d027b3448b6d7b6ddabd99cc7
kauã <profissional.kaua@gmail.com>
2023-07-24 11:12:54 -0300

    test: add dummy test fakeUserParams

---
+++ test/utils/fake-user-params.ts
@@ -0,0 +1,8 @@
+export const fakeUserParams = () => ({
+	name: "Josué",
+	lastname: "Manuel de Jesus",
+	cpf: "121.345.456-45",
+	email: "jose@gmail.com",
+	password: "123Agds.@",
+	passwordConfirmation: "123Agds.@",
+});
